c6b469f22907bc17401477588b9e16cbe0c5d2f1
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-04-25 10:22:30 +0200

    filtering animals

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./animals.json";
+import { useState } from "react";
+
+function clean(arr) {
+  return arr.map((animal) => {
+    "Mandu the amazing cat";
+    const [name, , desc, type] = animal.fullname.split(" ");
+    return {
+      name,
+      desc,
+      type,
+      age: animal.age,
+    };
+  });
+}
 
 function App() {
+  const animals = clean(data);
+  console.log(animals);
+  const [filter, setFilter] = useState("cat");
+  const [sort, setSort] = useState("name");
+
+  const filtered = filter === "" ? animals : animals.filter((animal) => animal.type === filter);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setFilter("cat")}>Cats</button>
+      <button onClick={() => setFilter("dog")}>Dogs</button>
+      <button onClick={() => setFilter("")}>All</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Desc</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filtered.map((animal) => {
+            return (
+              <tr>
+                <td>{animal.name}</td>
+                <td>{animal.type}</td>
+                <td>{animal.desc}</td>
+                <td>{animal.age}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
